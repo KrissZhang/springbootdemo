3ce43da04fc586b01ad7cbbffb141005fd13182c
zp <273629304@qq.com>
2019-06-23 21:05:14 +0800

    sql修改

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -1,7 +1,7 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : test
+ Source Server         : localhost
  Source Server Type    : MySQL
  Source Server Version : 50561
  Source Host           : localhost:3306
@@ -11,7 +11,7 @@
  Target Server Version : 50561
  File Encoding         : 65001
 
- Date: 31/05/2019 15:51:42
+ Date: 23/06/2019 21:03:59
 */
 
 SET NAMES utf8mb4;
@@ -22,10 +22,35 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `t_test`;
 CREATE TABLE `t_test`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `value` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+                         `id` int(11) NOT NULL AUTO_INCREMENT,
+                         `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+                         `value` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+                         PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of t_test
+-- ----------------------------
+INSERT INTO `t_test` VALUES (1, '1', '1');
+INSERT INTO `t_test` VALUES (2, '2', '2');
+INSERT INTO `t_test` VALUES (3, '3', '3');
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+                       `id` int(10) NOT NULL AUTO_INCREMENT,
+                       `uname` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+                       `pwd` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+                       PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, 'zhangsan', '202cb962ac59075b964b07152d234b70');
+INSERT INTO `user` VALUES (2, 'lisi', '202cb962ac59075b964b07152d234b70');
+INSERT INTO `user` VALUES (3, 'wangwu', '202cb962ac59075b964b07152d234b70');
 
 SET FOREIGN_KEY_CHECKS = 1;
