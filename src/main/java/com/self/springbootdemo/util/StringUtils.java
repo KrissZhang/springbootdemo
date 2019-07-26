569b1d21c99b6f72710c8c83766eec17156072b7
zpzp0813 <273629304@qq.com>
2019-07-26 14:49:12 +0800

    字符处理

--- src/main/java/com/self/springbootdemo/util/StringUtils.java
+++ src/main/java/com/self/springbootdemo/util/StringUtils.java
@@ -8,4 +8,12 @@ import cn.hutool.core.util.StrUtil;
  */
 public class StringUtils extends StrUtil {
 
+    /**
+     * 模糊匹配关键字符处理
+     * @return 处理后的关键字符
+     */
+    public static String addLikeSymbol(String keyword){
+        return "%" + keyword + "%";
+    }
+
 }
