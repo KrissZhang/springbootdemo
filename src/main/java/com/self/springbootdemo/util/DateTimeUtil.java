831e849d3e43133dbd16163b8a73668995eb3ded
zp <273629304@qq.com>
2019-08-11 21:25:57 +0800

    添加日期和时间工具方法

--- src/main/java/com/self/springbootdemo/util/DateTimeUtil.java
+++ src/main/java/com/self/springbootdemo/util/DateTimeUtil.java
@@ -1,11 +1,32 @@
 package com.self.springbootdemo.util;
 
 import cn.hutool.core.date.DateTime;
+import com.self.springbootdemo.constant.Constant;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 
 /**
  * 时间工具类
  * @author zp
  */
 public class DateTimeUtil extends DateTime {
+
+    /**
+     * 时间格式
+     */
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    /**
+     * 时间戳转时间字符串
+     * @param timestamp 时间戳
+     * @return 时间字符串
+     */
+    public static String timestampToStr(long timestamp){
+        LocalDateTime localDateTime = Instant.ofEpochMilli(timestamp).atZone(ZoneOffset.ofHours(Constant.Number.EIGHT)).toLocalDateTime();
+        return localDateTime.format(formatter);
+    }
 
 }
--- src/main/java/com/self/springbootdemo/util/DateUtil.java
+++ src/main/java/com/self/springbootdemo/util/DateUtil.java
@@ -1,9 +1,31 @@
 package com.self.springbootdemo.util;
 
+import com.self.springbootdemo.constant.Constant;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
 /**
  * 日期工具类
  * @author zp
  */
 public class DateUtil extends cn.hutool.core.date.DateUtil {
+
+    /**
+     * 日期格式
+     */
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    /**
+     * 时间戳转日期字符串
+     * @param timestamp 时间戳
+     * @return 日期字符串
+     */
+    public static String timestampToStr(long timestamp){
+        LocalDate localDate = Instant.ofEpochMilli(timestamp).atZone(ZoneOffset.ofHours(Constant.Number.EIGHT)).toLocalDate();
+        return localDate.format(formatter);
+    }
 
 }
